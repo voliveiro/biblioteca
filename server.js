a43dca68de16edd1c37e416b475cd67dd4bba406
Vernie <vernie.oliveiro@gmail.com>
2021-02-16 15:49:08 +0800

    updating server.js to add mongodb

--- server.js
+++ server.js
@@ -1,31 +1,63 @@
 require('dotenv').config(); 
-const express = require('express'); 
+const express = require('express');
+const app = express();
 const ejs = require('ejs');
 const methodOverride = require('method-override');
-
-const app = express()
+const Books = require('./models/books.js')
 const data = require('./models/data.js')
+const mongoose = require('mongoose');
+const mongoURI = process.env.DB_URI ||'mongodb://localhost:27017/'+ 'books'
+const db = mongoose.connection
 
-/* Middleware */ 
+
 app.use(express.static('public'));
-app.use(express.json()); 
-app.use(express.urlencoded({extended:false})); 
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use((req, res, next) => {
-    console.log ('Middleware'); 
+    console.log('Middleware 2');
     next();
-})
+});
 
-//display data
-app.get('/', (req, res) => {
-    res.send(data)
-})
+
+ 
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(
+	mongoURI,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => {
+		console.log('the connection with mongod is established');
+	}
+);
+
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo');
+});
+
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', mongoURI))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+//add seed data
+
+// app.get('/index/seed', (req, res) => {
+//     Books.create(data, (err, data) => {
+//         res.redirect('/');
+//     })
+// });
+
+//show all books
 
 app.get('/index', (req, res) => {
-    res.render('index.ejs', {
-        books: data,
-    })
-})
+    Books.find({}, (error, books) => {
+        res.render('index.ejs', {
+            Books: books
+        });
+    });
+});
+
 
 //SHOW 
 
