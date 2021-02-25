6ac7e0c8ada13f72db344935b36c1921606ef7c1
Vernie <vernie.oliveiro@gmail.com>
2021-02-25 18:36:22 +0800

    adding users model

--- models/authors.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const authorSchema = mongoose.Schema({
-	name: String
-});
-
-const Author = mongoose.model('Author', authorSchema);
-
-module.exports = Author;
