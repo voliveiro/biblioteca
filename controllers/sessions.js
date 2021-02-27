19bb20f223ce46f331ac8f95be06736b5189512c
Vernie <vernie.oliveiro@gmail.com>
2021-02-27 09:56:58 +0800

    got bcrypt to work

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -6,30 +6,41 @@ const User = require('../models/users.js')
 sessions.get('/new', (req, res) => {
   res.render('sessions/new.ejs')
 })
-sessions.post('/', (req, res) => {
-  User.findOne({username: req.body.username}, (err, foundUser) => {
-  // if db error handle the db error
-    if(err) {
-      console.log(err)
-      res.send('oops something went wrong')
-      // if user not found, handle the error
-    } else if (!foundUser) {
-      res.send('user not found!')
-    }else {
-      if(req.body.password == foundUser.password) {
-        req.session.currentUser = foundUser
-        res.redirect('/')
-        // if passwords don't match, handle the error
-      } else {
-        res.send('<a href="/">wrong password</a>')
-      }
-    }
-  })
-})
+// sessions.post('/', (req, res) => {
+//   User.findOne({username: req.body.username}, (err, foundUser) => {
+//   // if db error handle the db error
+//     if(err) {
+//       console.log(err)
+//       res.send('oops something went wrong')
+//       // if user not found, handle the error
+//     } else if (!foundUser) {
+//       res.send('user not found!')
+//     }else {
+//       if(bcrypt.compareSync(req.body.password == foundUser.password)) {
+//         req.session.currentUser = foundUser
+//         res.redirect('/')
+//         // if passwords don't match, handle the error
+//       } else {
+//         res.send('<a href="/">wrong password</a>')
+//       }
+//     }
+//   })
+// })
 
 sessions.delete('/', (req, res) => {
   req.session.destroy(()=>{
     res.redirect('/')
+  })
+})
+
+sessions.post('/', (req, res) => {
+  User.findOne({ username: req.body.username }, (err, foundUser) => {
+    if( bcrypt.compareSync(req.body.password, foundUser.password) ){
+        req.session.currentUser = foundUser;
+          res.redirect('/');
+      } else {
+          res.send('<a href="/">wrong password</a>');
+      }
   })
 })
 
--- controllers/users.js
+++ controllers/users.js
@@ -18,14 +18,23 @@ users.get('/new',  (req, res) => {
 
 // create a new user based on input in form
 
-users.post('/', (req, res) => {
-  User.create(req.body, (err, createdUser) => {
-    if (err) {
-      console.log(err)
-    }
-    console.log(createdUser);
-    res.redirect('/')
-  })
-}) 
+// users.post('/', (req, res) => {
+//   req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+//   User.create(req.body, (err, createdUser) => {
+//     if (err) {
+//       console.log(err)
+//     }
+//     console.log(createdUser);
+//     res.redirect('/')
+//   })
+// }) 
+
+users.post('/', (req, res)=>{
+  //overwrite the user password with the hashed password, then pass that in to our database
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+    User.create(req.body, (err, createdUser)=>{
+        res.redirect('/')
+    })
+})
 
 module.exports = users
